5178f2cb937f7db6984e1cdc964ee41a6618d35b
Meindert <meindertduin@hotmail.com>
2020-10-14 16:45:27 +0200

    setting up js for screensharer

---
+++ Asp Net Core/AspNetCoreExercises/ScreenShareApplication/WebClient/wwwroot/js/screenRecorder.js
@@ -0,0 +1,21 @@
+﻿window.screenCapture = {
+    async startCapture(displayMediaOption){
+        let captureStream = null;
+        console.log(displayMediaOption);
+        try {
+            captureStream = await navigator.mediaDevices.getUserMedia(displayMediaOption);
+        }catch (err) {
+            console.log(err);
+        }
+        return captureStream;
+    },
+    async getConnectedDevices(type, callback){
+        navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+                const filtered = devices.filter(devices => devices.kind === type);
+            }).catch(err => console.log(err));
+    },
+    async displayConnectedDevices(){
+        await this.getConnectedDevices('videoInput', cameras => console.log(cameras));
+    } 
+}
